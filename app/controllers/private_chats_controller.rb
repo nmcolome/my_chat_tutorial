5725dc6852fe4d609e9577b21bc8c142c4cad217
NataliaColome <nmcolome@gmail.com>
2017-09-10 17:22:09 -0600

    Change collection of form_helper to User.all.order(:username) and add functionality to private_chats#create

--- app/controllers/private_chats_controller.rb
+++ app/controllers/private_chats_controller.rb
@@ -4,6 +4,8 @@ class PrivateChatsController < ApplicationController
   end
 
   def create
-    
+    @participant_ids = params[:participant_ids].select(&:present?) << current_user.id.to_s
+    @private_chat = PrivateChat.create(participant_ids: @participant_ids)
+    redirect_to private_chat_path(@private_chat)
   end
 end
