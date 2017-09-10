aaf8bf1c41e218b55426617eff608e9125cd64a6
NataliaColome <nmcolome@gmail.com>
2017-09-10 02:26:05 -0600

    Add link to 'open a private chat',  create route and controller for private_chats#new and create; and create view and form for private_chats#new

---
+++ app/controllers/private_chats_controller.rb
@@ -0,0 +1,9 @@
+class PrivateChatsController < ApplicationController
+  def new
+    @private_chat = PrivateChat.new
+  end
+
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#delete"
 
   resources :users, only: [:new, :create]
+  resources :private_chats, only: [:new, :create]
 end
