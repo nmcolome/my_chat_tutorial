c0818b982f33d3a901a03f78dedeba847905709c
NataliaColome <nmcolome@gmail.com>
2017-09-09 20:11:04 -0600

    Add sad paths to user_can_sign_up in case it doesn't have a password or a name. Add validation to User model. Add flash message to users#create and save a session when a user signs up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to :root
     else
-      # redirect if something went wrong
+      flash[:error] = "Invalid username or password"
+      redirect_to new_user_path
     end
   end
 
--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -10,7 +10,35 @@ RSpec.describe "a user can signup" do
     fill_in "Password", with: "password"
     click_on "Sign Up"
 
-    expect(current_path).to eq("/")
     expect(User.count).to eq(1)
+    expect(current_path).to eq("/")
+  end
+
+  it "without a password" do
+    visit "/"
+    click_on "Sign Up"
+
+    expect(current_path).to eq("/users/new")
+    fill_in "Username", with: "test_user"
+    fill_in "Password", with: ""
+    click_on "Sign Up"
+
+    expect(User.count).to eq(0)
+    expect(current_path).to eq(new_user_path)
+    expect(page).to have_content("Invalid username or password")
+  end
+
+  it "without a name" do
+    visit "/"
+    click_on "Sign Up"
+
+    expect(current_path).to eq("/users/new")
+    fill_in "Username", with: ""
+    fill_in "Password", with: "password"
+    click_on "Sign Up"
+
+    expect(User.count).to eq(0)
+    expect(current_path).to eq(new_user_path)
+    expect(page).to have_content("Invalid username or password")
   end
 end
