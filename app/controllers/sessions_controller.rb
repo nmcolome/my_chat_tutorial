56a598dfdc6108738936c54a5ec852d240651ee0
NataliaColome <nmcolome@gmail.com>
2017-09-09 19:37:42 -0600

    Add post action as sessions#create route and build functionality to validate credentials and store a session. Pass user_can_login_spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,14 @@ class SessionsController < ApplicationController
   def new
     
   end
+
+  def create
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to :root
+    else
+      # do something if invalid credentials
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
   
   resources :users, only: [:new, :create]
 end
