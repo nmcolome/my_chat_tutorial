482499b00d8af78ed68ef6767f5769517fe4ff4b
NataliaColome <nmcolome@gmail.com>
2017-09-09 19:20:58 -0600

    Fix text on Sign up link

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "a user can signup" do
   it "with valid credentials" do
     visit "/"
-    click_on "signup"
+    click_on "Sign Up"
 
     expect(current_path).to eq("/users/new")
     fill_in "Username", with: "test_user"
