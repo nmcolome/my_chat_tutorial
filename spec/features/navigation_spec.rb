550a4ebf9d225baf7a353a6604e85242f0f677f0
NataliaColome <nmcolome@gmail.com>
2017-09-09 23:41:15 -0600

    Fix test for navigation as a logged in user and add if statement to pass test

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -12,6 +12,9 @@ RSpec.describe "Navigation" do
 
   context "as a logged in user" do
     it "the Sign Up/ Login link are not there" do
+      user = User.create(username: "test_user", password: "password")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       visit "/"
       expect(page).to have_link("Logout")
       expect(page).to_not have_link("Sign Up")
