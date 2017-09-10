660a5f553df20d3c97089998cce9b4b346ca0a8c
NataliaColome <nmcolome@gmail.com>
2017-09-09 19:17:02 -0600

    Create user_can_login_spec

---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "a user can login" do
+  it "with valid credentials" do
+    user = User.create(username: "test_user", password: "password")
+
+    visit "/"
+    click_on "Login"
+
+    fill_in "Username", with: "test_user"
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    expect(current_path).to eq("/")
+  end
+end
