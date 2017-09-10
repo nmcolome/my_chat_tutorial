164b2e09f16bb777abb1bd0cc048d8935abbf0e4
NataliaColome <nmcolome@gmail.com>
2017-09-09 19:07:06 -0600

    Add user#create route and functionality. Include bcrypt in gemfile and bundle. Pass user_can_sign_up_spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to :root
+    else
+      # redirect if something went wrong
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 end
